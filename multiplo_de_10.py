159065e20d43400f5c8588e5de7ad5bf474e6f32
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-02-21 07:39:22 -0600

    First

--- multiplo_de_10.py
+++ multiplo_de_10.py
@@ -1,15 +1,26 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+• Multiplo de 10
+• Jesús Alejandro Sánchez Muñoz
+• 758377
+• Igenieria Mecanica
+• Algoritmos y Programcion
+• ESI232B2
+• Jorge Zaldivar
+• 21/feb/25
+• 5 min
 """
 
 # Declaraciones
-CONSTANTE = valor
+
 
 # Entradas
-entrada = input()
+A = int(input("Ingrese el nuemro que desaria ver si es multiplo de 10: \n"))
+if A % 10 == 0:
+    print("Su numero si es divisible entre 10")
+else:
+    print("Su numero no es divisible entre 10")
 
 # Proceso
 
 
 # Salidas
-print(salida)
